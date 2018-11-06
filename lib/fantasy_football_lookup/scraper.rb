7bf2c2efc75be866b98e0126c1406c9c16fb01b1
Chris Tompkins <chtompkins026@gmail.com>
2018-11-06 14:56:53 -0500

    Modified Scraper

--- lib/fantasy_football_lookup/scraper.rb
+++ lib/fantasy_football_lookup/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
 @@ALL = [["|Name|","|Pos|","|Team|", "|Opp|"],[" ", " ", " "," "]]
 @@IDS = ["data-name","data-position","data-team","data-opp"]
 
+
   def self.scrape_index_page(player_link) #pulls the ranking of each player
     doc = Nokogiri::HTML(open(@link))
     players = doc.css('.player-row')
@@ -21,6 +22,26 @@ class Scraper
     show_all
     end
   end #scrape_index_page
+
+  def self.player_description(name = nil) #returns the player description
+    if name.nil?
+
+      doc = Nokogiri::HTML(open(@link))
+      players = doc.css('.player-row')
+
+      puts "Pick a name to get a description: "
+      name = gets.chomp
+      player_link = players.detect do |p|
+        p.css("a").attribute("href").value.include? name.downcase.gsub(" ","-")
+      end
+      doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/players/" + player_link.css("a").attribute("href")))
+    else
+      name = name.gsub(" ", "-").downcase
+      doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/players/#{name}.php"))
+    end
+
+    return display_ranking(doc)
+  end
 
 
 end #end of the Scraper Class
