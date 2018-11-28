eac208e309413fa5bdefb3cec3f767f1b7f1f54e
Chris Tompkins <chtompkins026@gmail.com>
2018-11-28 12:57:55 -0500

    Typo fix on player class

--- lib/fantasy_football_lookup/player.rb
+++ lib/fantasy_football_lookup/player.rb
@@ -37,6 +37,7 @@ class Player
     #    Override the to string method to print out the strig representation
     #    of the player per every object
     def overview
+        puts ""
         "Name: #{@name}\nProjected Score: #{@score}\nPosition: #{@position}\nWeek: #{team}\n"
     end
 
